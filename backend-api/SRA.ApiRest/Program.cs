2ff1f06c54718df333e5ff0d0f5a8d3c5319c7cb
Dennis <dennysnob98@gmail.com>
2025-05-28 15:18:22 +0200

    Fase 2: Migracion base

--- backend-api/SRA.ApiRest/Program.cs
+++ backend-api/SRA.ApiRest/Program.cs
@@ -4,34 +4,28 @@ using SRA.ApiRest.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SraConnection")));
-
-// Add services to the container.
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(p => p.AddPolicy("CorsPolicy", build =>
 {
-    //modify URL with required domain of the front-end app
     build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseCors("CorsPolicy");
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
