f630d7f0f43e2e9909aeb6051ff646dc0f8ea5d0
Dennis <dennysnob98@gmail.com>
2025-05-28 12:18:35 +0200

    Fase 1 completada: API base

---
+++ backend-api/SRA.ApiRest/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SRA.ApiRest.Models.Entity;
+
+namespace SRA.ApiRest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Reserva> Reservas => Set<Reserva>();
+    }
+}
---
+++ backend-api/SRA.ApiRest/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using SRA.ApiRest.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SraConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(p => p.AddPolicy("CorsPolicy", build =>
+{
+    //modify URL with required domain of the front-end app
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
